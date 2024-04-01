f3dd7143e4fcaa4f7c190a123296d1dc70456aff
Walker Sundquist <wwsundquist@gmail.com>
2024-03-31 17:14:44 -0700

    start fit by product for two gaussian

--- src/sec_vertices.py
+++ src/sec_vertices.py
@@ -28,17 +28,42 @@ xMax = 0.52
 bottom = 0.0
 top = 4300.0
 
-# naïve gaussian
+# naive gaussian
 fitMin = ROOT.TLine(xMin, bottom, xMin, top)
 fitMax = ROOT.TLine(xMax, bottom, xMax, top)
 fitMin.SetLineColor(ROOT.kTeal)
 fitMin.SetLineStyle(2)
 fitMax.SetLineColor(ROOT.kTeal)
 fitMax.SetLineStyle(2)
-gausFit = ksmass_hist.Fit("gaus","SR", "", xMin, xMax)
+#gausFit = ksmass_hist.Fit("gaus","SR", "", xMin, xMax)
 
-#product of two gaussian
-gausProd = ROOT.TF1("gausProd", "", xMin, xMax)
+
+#Product of two gaussian
+
+# Parameter naming convention
+# [0] : mu_1, mean of 1st gaussian
+# [1] : sigma_1, std. dev. of 1st gaussian
+# [2] : mu_2, mean of 2nd gaussian
+# [3] : sigma_2, std. dev. of 2nd gaussian
+
+def gausProd_func(x, params):
+	pi = 3.1415926535897932385
+	e = 2.7182818284590452356
+	expArg = (-1.0/2)*( ( (x[0] - params[0])/(params[1]) )**2 + ( (x[0] - params[2])/(params[3]) )**2 )
+	prefac = 1.0/(2*pi*params[1]*params[3])
+	res = prefac * (e**expArg)
+	return res
+
+# Initialize parameters
+init_mu_1 = 0.0
+init_sigma_1 = 1.0
+init_mu_2 = 0.0
+init_sigma_2 = 1.0
+
+gausProd = ROOT.TF1("gausProd", gausProd_func, xMin, xMax, 4)
+gausProd.SetParameters(init_mu_1, init_sigma_1, init_mu_2, init_sigma_2)
+
+ksmass_hist.Fit(gausProd, "SR", "", xMin, xMax)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
