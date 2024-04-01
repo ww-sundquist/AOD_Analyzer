8908c8a9d773f3eff6a16d026eb7243fdeacff1f
Walker Sundquist <wwsundquist@gmail.com>
2024-03-31 21:37:15 -0700

    Add fit to product of two gaussians

--- src/sec_vertices.py
+++ src/sec_vertices.py
@@ -23,49 +23,53 @@ c = ROOT.TCanvas( "c", "c", 800, 800 )
 
 # Fits # # # # # # # # # # # # # # # # # # # # # # # #
 
-xMin = 0.48#restrict domain of fit, values taken by eye
-xMax = 0.52
+xMin = 0.49#restrict domain of fit, values taken by eye
+xMax = 0.51
 bottom = 0.0
 top = 4300.0
 
-# naive gaussian
+# # Naive gaussian # # #
 fitMin = ROOT.TLine(xMin, bottom, xMin, top)
 fitMax = ROOT.TLine(xMax, bottom, xMax, top)
 fitMin.SetLineColor(ROOT.kTeal)
 fitMin.SetLineStyle(2)
 fitMax.SetLineColor(ROOT.kTeal)
 fitMax.SetLineStyle(2)
-#gausFit = ksmass_hist.Fit("gaus","SR", "", xMin, xMax)
+#ksmass_hist.Fit("gaus","SR", "", xMin, xMax)
 
-
-#Product of two gaussian
+# # Product of two gaussians # # #
 
 # Parameter naming convention
 # [0] : mu_1, mean of 1st gaussian
 # [1] : sigma_1, std. dev. of 1st gaussian
 # [2] : mu_2, mean of 2nd gaussian
 # [3] : sigma_2, std. dev. of 2nd gaussian
+# [4] : y_inf, y-offset at infinity
+# [5] : N, scale factor
 
 def gausProd_func(x, params):
 	pi = 3.1415926535897932385
 	e = 2.7182818284590452356
 	expArg = (-1.0/2)*( ( (x[0] - params[0])/(params[1]) )**2 + ( (x[0] - params[2])/(params[3]) )**2 )
-	prefac = 1.0/(2*pi*params[1]*params[3])
-	res = prefac * (e**expArg)
+	prefac = 1.0*params[5]/(2*pi*params[1]*params[3])
+	res = ( prefac * (e**expArg) ) + params[4]
 	return res
 
 # Initialize parameters
-init_mu_1 = 0.0
+init_mu_1 = 0.5
 init_sigma_1 = 1.0
-init_mu_2 = 0.0
+init_mu_2 = 0.5
 init_sigma_2 = 1.0
+init_y_inf = 750.0
+init_N = 1.0
 
-gausProd = ROOT.TF1("gausProd", gausProd_func, xMin, xMax, 4)
-gausProd.SetParameters(init_mu_1, init_sigma_1, init_mu_2, init_sigma_2)
+gausProd = ROOT.TF1("gausProd", gausProd_func, xMin, xMax, 6)
+gausProd.SetParameters(init_mu_1, init_sigma_1, init_mu_2, init_sigma_2, init_y_inf, init_N)
+gausProd.SetLineColor(ROOT.kRed)
 
 ksmass_hist.Fit(gausProd, "SR", "", xMin, xMax)
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
 ksmass_hist.Draw()
