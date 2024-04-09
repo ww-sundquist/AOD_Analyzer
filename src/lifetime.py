65e933220003ceca468586295dd075b01a92c370
Walker Sundquist <wwsundquist@gmail.com>
2024-04-09 11:56:40 -0700

    try other fits to gammabeta

--- src/lifetime.py
+++ src/lifetime.py
@@ -1,6 +1,7 @@
 import DataFormats.FWLite as fwlite
 import ROOT
 import math
+import numpy as np
 
 events =		fwlite.Events("file:output.root")
 secondaryVertices =	fwlite.Handle("std::vector<reco::VertexCompositeCandidate>")
@@ -22,8 +23,8 @@ for event in events:
 
 # # # # # # # # # # # # # # # # # # # # # #
 
-xMin = 0.0#restrict domain of fit, values taken by eye
-xMax = 40.0
+xMin = 1.0#restrict domain of fit, values taken by eye
+xMax = 55.0
 
 #Fit beta gamma dist'n to f(x) = (A exp(-x) x) - (B exp(-x^2) x) - (C exp(-x) x^2) - (D exp(-x^2) x^2) - (E exp(-x^2) x^3) + (F exp(-x) x^4)
 
@@ -56,7 +57,43 @@ def banerjee_func(x, params):
 banerjee = ROOT.TF1("banerjee", banerjee_func, xMin, xMax, 6)
 #banerjee.SetParameters(init_A, init_B, init_C, init_D, init_E, init_F)
 
-ksbetagamma.Fit(banerjee, "SR", "", xMin, xMax)
+#ksbetagamma.Fit(banerjee, "SR", "", xMin, xMax)
+
+
+#Fit beta gamms dist'n to skewed gaussian
+
+def skewed_normal_func(x, params):
+	loc = params[0]
+	scale = params[1]
+	skew = params[2]
+	
+	arg1 = (skew / (scale * math.sqrt(2)))*(x[0] - loc) #argument of erf
+	arg2 = (-1.0 / (2 * scale**2))*(x[0] - loc)**2 #argument of exp
+	coeff = 1.0 / (scale * math.sqrt(2*math.pi))
+	res = coeff * (1 - math.erf(arg1)) * math.exp(arg2)
+	return res
+
+skewed_normal = ROOT.TF1("skewed_normal", skewed_normal_func, xMin, xMax, 4)
+#skewed_normal.SetParameters(1.0, 1.0, 1.0, 1.0)
+
+#ksbetagamma.Fit(skewed_normal, "SR", "", xMin, xMax)
+
+
+#Planck's law-type function
+def planck_func(x, params):
+	a = params[0]
+	b = params[1]
+	c = 1.0#params[2]
+	d = 0.0#params[3]
+	k = 0.5
+
+	res = (   a*pow(x[0]+k,3) / (  np.exp( b*(x[0] + k) ) - c  )   ) + d
+	return res
+
+planck = ROOT.TF1("planck", planck_func, xMin, xMax, 2)
+planck.SetParameters(100.0,100.0,0.0)
+ksbetagamma.Fit(planck, "SR", "", xMin, xMax)
+
 
 # # # # # # # # # # # # # # # # # # # # # #
 
